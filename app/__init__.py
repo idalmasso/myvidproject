63ada2ffb2e4cc365789a9be8a0aefbbc5b795fd
idalmasso <ivano.dalmasso@gmail.com>
2018-04-09 23:00:24 +0200

    Logs

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_bootstrap import Bootstrap
 from pymongo import ASCENDING
 from config import Config
 from threading import Thread
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 mongo = PyMongo()
 login = LoginManager()
@@ -31,8 +34,23 @@ def create_app(config_class=Config):
     from app.videoapp import bp as videoapp_bp
     app.register_blueprint(videoapp_bp)
     Thread(target=video.Video.video_download_procedure, args=(app,)).start()
+    if not app.debug:
+        # ...
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/vid.log', maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('log startup')
     return app
 
 from app import usermodel, video
+
 
 
